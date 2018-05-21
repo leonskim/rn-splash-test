30cc806173524824d9b22c8d5d7a767fbab6b1d4
Leon Kim <minusnine@gmail.com>
2018-05-21 14:40:35 +0900

    Add: initialize

--- android/app/src/main/java/com/leontest/MainActivity.java
+++ android/app/src/main/java/com/leontest/MainActivity.java
@@ -1,8 +1,17 @@
 package com.leontest;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import com.cboy.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- ios/LeonTest/AppDelegate.m
+++ ios/LeonTest/AppDelegate.m
@@ -6,6 +6,7 @@
  */
 
 #import "AppDelegate.h"
+#import "SplashScreen.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -29,6 +30,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [SplashScreen show];
   return YES;
 }
 
