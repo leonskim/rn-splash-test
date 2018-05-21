ebb0c0c79e5187fe7c7adbec54d296c109decfef
Leon Kim <minusnine@gmail.com>
2018-05-21 16:29:25 +0900

    Remove: old package reference

--- android/app/src/main/java/com/leontest/MainActivity.java
+++ android/app/src/main/java/com/leontest/MainActivity.java
@@ -3,7 +3,6 @@ package com.leontest;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
-import com.cboy.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
